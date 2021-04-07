fc0f8ee58266aa0281e374541350c785f6ab5893
AshwinJega <jegatheesanashwin@gmail.com>
2021-04-07 14:12:05 -0400

    Local

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post('/api/login', async (req, res, next) => 
+{
+  // incoming: login, password
+  // outgoing: id, firstName, lastName, error
+
+  var error = '';
+
+  const { login, password } = req.body;
+
+  var id = -1;
+  var fn = '';
+  var ln = '';
+
+  if( login.toLowerCase() == 'rickl' && password == 'COP4331' )
+  {
+    id = 1;
+    fn = 'Rick';
+    ln = 'Leinecker';
+  }
+  else
+  {
+    error = 'Invalid user name/password';
+  }
+
+  var ret = { id:id, firstName:fn, lastName:ln, error:error};
+  res.status(200).json(ret);
+});
+
+
+app.use((req, res, next) => 
+{
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, DELETE, OPTIONS'
+  );
+  next();
+});
+
+app.listen(5000); // start Node + Express server on port 5000
