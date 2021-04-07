902a845446c44b08866dc94801d5378a121987db
AshwinJega <jegatheesanashwin@gmail.com>
2021-04-07 14:38:56 -0400

    Heroku changes

--- server.js
+++ server.js
@@ -5,8 +5,20 @@ const path = require('path');
 const PORT = process.env.PORT || 5000;  
 
 const app = express();
+app.set('port', (process.env.PORT || 5000));
 app.use(cors());
 app.use(bodyParser.json());
+
+if (process.env.NODE_ENV === 'production') 
+{
+  // Set static folder
+  app.use(express.static('frontend/build'));
+
+  app.get('*', (req, res) => 
+ {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+}
 
 app.post('/api/login', async (req, res, next) => 
 {
@@ -50,6 +62,7 @@ app.use((req, res, next) =>
   );
   next();
 });
+
 
 app.listen(PORT, () => 
 {
